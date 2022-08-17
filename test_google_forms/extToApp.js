63b247ec3df901610d4bf7e3bc43798a5a711800
David Melsheimer <dave.melshman@gmail.com>
2022-08-17 14:02:50 -0400

    getting entries into sheet from API

--- test_google_forms/extToApp.js
+++ test_google_forms/extToApp.js
@@ -1,9 +1,31 @@
-function testGS(){const url = 'https://script.google.com/macros/s/AKfycbwkRmTyKbztC9Trgb9GEIwR1QtIhjQiWadjH8Tjpt9DeDvhT4_nCmNknpNIUJEU4Sx-mQ/exec';
+const url = 'https://script.google.com/macros/s/AKfycbyXjDsf9R3fznkbLSZNfIpr5TVYgwY6skAQGmfVDiHvfb28FhPYFYJUnrYY5eRzkFfpNA/exec';
+
+function testGS(){
     fetch(url)
         .then( d => d.json())
         .then(d => { 
             document.getElementById('app_return_text').textContent = d[0].status;
+            document.getElementById('reply').textContent = d[0].thoughtfulReply;
         });
 }
 
+function testDoPost(){
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: 'same-origin', // include, *same-origin, omit
+        // headers: {
+        //   'Content-Type': 'application/json'
+        //   // 'Content-Type': 'application/x-www-form-urlencoded',
+        // },
+        redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name: "Patrick"}) // body data type must match "Content-Type" header
+      });
+}
+
+
+
 document.getElementById('btn').addEventListener('click', testGS);
+document.getElementById('btn2').addEventListener('click', testDoPost);
